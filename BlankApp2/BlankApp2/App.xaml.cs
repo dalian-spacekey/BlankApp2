f2b9584def4b5ac840c88a20ce28a01e10814748
spacekey <fumihiro.fukuda@gmail.com>
2020-01-31 21:17:20 +0800

    Add project files.

---
+++ BlankApp2/BlankApp2/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using Prism;
+using Prism.Ioc;
+using BlankApp2.ViewModels;
+using BlankApp2.Views;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace BlankApp2
+{
+    public partial class App
+    {
+        public App() : this(null) { }
+
+        public App(IPlatformInitializer initializer) : base(initializer) { }
+
+        protected override async void OnInitialized()
+        {
+            InitializeComponent();
+
+            await NavigationService.NavigateAsync("NavigationPage/MainPage");
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            containerRegistry.RegisterForNavigation<NavigationPage>();
+            containerRegistry.RegisterForNavigation<MainPage, MainPageViewModel>();
+        }
+    }
+}
